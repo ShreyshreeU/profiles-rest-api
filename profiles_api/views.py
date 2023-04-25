104479c751f3c00a075dc909baedb5b14b2b2382
Shreyshree <shreyc.2699@gmail.com>
2023-04-25 09:00:00 +0530

    enable Django admin for user profile model

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from profiles_api import serializers
+
+
+class HelloApiView(APIView):
+    """Test API View"""
+    serializer_class = serializers.HelloSerializer
+
+    def get(self, request, format=None):
+        """Returns alist of APIViews features"""
+
+        an_apiview = [
+            'uses HTTP methods as function (get, post, patch, put, delete)',
+            'Is similar to a traditional Django View',
+            'Gives you most control over you application logic',
+            'Is mapped manually to URLs',
+        ]
+
+        return Response({'message': 'Hello', 'an_apiview': an_apiview})
+
+    def posst(self, request):
+        """Create a hello message with our names"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST 
+            )
+        
+    def put(self, request, pk=None):
+        """Handle updating an object"""
+        return Response({'method': 'PUT'})
+    
+    def patch(self, request, pk=None):
+        """handle a praticle update of an object"""
+        return Response({'method':'PATCH'})
+    
+    def delete(self, request, pk=None):
+        """delete an object"""
+        return Response({'method': 'DELETE'})
